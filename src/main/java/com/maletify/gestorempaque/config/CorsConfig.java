72b7d2d03ad979e004ba82be2aa98a8959e46d81
krispirate <durandvillac@gmail.com>
2025-12-02 23:34:38 -0500

    Update CorsConfig.java Vercel en cors

--- src/main/java/com/maletify/gestorempaque/config/CorsConfig.java
+++ src/main/java/com/maletify/gestorempaque/config/CorsConfig.java
@@ -14,7 +14,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000",
+                                       "https://maletify-frontend.vercel.app"
+                                       )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
